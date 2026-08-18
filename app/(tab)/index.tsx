95880e3731eecda1bd2a58dac44dd4655e26fe91
sophiathapa <sophia.thapa55@gmail.com>
2026-08-18 19:15:55 +0545

    implements fonts

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -8,7 +8,7 @@ export default function App() {
   };
   return (
     <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">Welcome to Nativewind!</Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
       <View className="flex flex-col gap-5">
         <Link href="/homepage" className="bg-primary text-white p-5 rounded-2xl">
           Go to homePage
